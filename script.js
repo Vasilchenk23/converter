f60f04c7ae9a7c7d21322ab010b8fb8c03368246
Vasilchenk23 <vasilchenko.maksim374@gmail.com>
2024-07-27 12:35:51 +0300

    create two tables current

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 async function convertCurrency() {
     const apiKey = '7525fa8510e2984db056ca91';
     const amount = document.getElementById('amount').value;
@@ -20,3 +19,25 @@ async function convertCurrency() {
         alert(`Error: ${error}`);
     }
 }
+
+async function fetchRates() {
+    const apiKey = '7525fa8510e2984db056ca91';
+    try {
+        const fiatResponse = await fetch(`https://v6.exchangerate-api.com/v6/${apiKey}/latest/USD`);
+        const fiatData = await fiatResponse.json();
+
+        const cryptoResponse = await fetch('https://min-api.cryptocompare.com/data/price?fsym=USD&tsyms=BTC,ETH');
+        const cryptoData = await cryptoResponse.json();
+
+        document.getElementById('usd-buy-rate').innerText = fiatData.conversion_rates.EUR.toFixed(2);
+        document.getElementById('usd-sell-rate').innerText = fiatData.conversion_rates.UAH.toFixed(2);
+        document.getElementById('eur-buy-rate').innerText = fiatData.conversion_rates.USD.toFixed(2);
+        document.getElementById('eur-sell-rate').innerText = fiatData.conversion_rates.UAH.toFixed(2);
+        // document.getElementById('usdt-buy-rate').innerText = cryptoData.BTC.toFixed(2);
+        // document.getElementById('usdt-sell-rate').innerText = cryptoData.ETH.toFixed(2);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', fetchRates);
